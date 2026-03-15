9df8caf834ae1d0ae74cd628f8a623bc7953f8ea
oksanatkach <o.tkach@spd.tech>
2026-03-15 16:00:45 +0200

    forgot config

--- conf/config.py
+++ conf/config.py
@@ -16,5 +16,5 @@ bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float32,  # ← this is the key change
+    bnb_4bit_compute_dtype=torch.float16,
 )
