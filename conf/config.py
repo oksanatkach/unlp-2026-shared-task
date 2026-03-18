c6b0334b46924940e188159af62cd88387f8a392
oksanatkach <o.tkach@spd.tech>
2026-03-18 22:31:29 +0200

    changed load method selection

--- conf/config.py
+++ conf/config.py
@@ -1,5 +1,3 @@
-from transformers import BitsAndBytesConfig
-import torch
 import os
 
 
@@ -11,10 +9,3 @@ retriever_path = os.getenv('RETRIEVER_PATH') or '../data/uk_hybrid_index_1000_e5
 embedding_model_name = os.getenv('EMBEDDING_MODEL_NAME') or 'intfloat/multilingual-e5-large'
 reranker_model_name = os.getenv('RERANKER_MODEL_NAME') or 'BAAI/bge-reranker-v2-m3'
 llm_model_name = os.getenv('LLM_MODEL_NAME') or 'lapa-llm/lapa-v0.1.2-instruct'
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-)
