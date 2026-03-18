6de4c9c379dc4990e98f64bf81305d928cb03342
oksanatkach <o.tkach@spd.tech>
2026-03-18 22:31:02 +0200

    changed load method selection

--- MCQA/device.py
+++ MCQA/device.py
@@ -8,17 +8,8 @@ device = get_accelerator()
 logger = logging.getLogger(__name__)
 
 load_method = None
-if config.USE_VLLM and 'A100' in device:
+if config.USE_VLLM and 'GPU' in device:
     logger.info(f'Device is {device}, loading VLLM model')
-    load_method = 'VLLM'
-
-elif 'TPU' in device:
-    if not config.USE_VLLM:
-        logger.error(f"Device is {device}, can't use transformers, loading VLLM model")
-    else:
-        logger.info(f'Device is {device}, loading VLLM model')
-
-    os.environ["PJRT_DEVICE"] = "TPU"
     load_method = 'VLLM'
 
 elif config.USE_VLLM:
--- MCQA/objects.py
+++ MCQA/objects.py
@@ -70,9 +70,16 @@ def load_llm():
         return llm
 
     else:
+        from transformers import BitsAndBytesConfig
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.float16,
+        )
         llm = Gemma3ForCausalLM.from_pretrained(
             config.llm_model_name,
-            quantization_config=config.bnb_config,
+            quantization_config=bnb_config,
             device_map="auto",
         )
 
