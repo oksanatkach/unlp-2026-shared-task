84ac2d3e28ca08d13e07e2d8b2c7a5b86b847f44
oksanatkach <o.tkach@spd.tech>
2026-03-18 23:40:00 +0200

    speeding up transformers

--- MCQA/objects.py
+++ MCQA/objects.py
@@ -75,11 +75,12 @@ def load_llm():
             load_in_4bit=True,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_compute_dtype=torch.bfloat16,
         )
         llm = Gemma3ForCausalLM.from_pretrained(
             config.llm_model_name,
             quantization_config=bnb_config,
+            attn_implementation="sdpa",
             device_map="auto",
         )
 
